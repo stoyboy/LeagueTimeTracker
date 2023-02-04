06cbbfa48030f21090a99804f29fe88d4aba3acb
Jovan Stojimirovic <jocas03@hotmail.com>
2023-02-04 22:21:25 +0100

    PEPE!!!!!!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/pepeDS.gif" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
